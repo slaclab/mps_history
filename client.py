645bdeef28d2b58c7b1af33b3c73ac3e1bf7689b
King <lking1106@gmail.com>
2021-11-15 10:42:45 -0800

    update test data

--- client.py
+++ client.py
@@ -84,14 +84,19 @@ def generate_test_data(env):
     #BypassStateType, AnalogDevice.id, oldValue, newValue, 0-31
     analog_bypass = [2, random.choice(result), random.randint(0,1), random.randint(0,1), random.randint(0, 31)]
     #BypassStateType, DeviceInput.id, oldValue, newValue, index(>31)
-    digital_bypass = [2, random.randint(1,1011), random.randint(0,1), random.randint(0,1), 32]
+    #digital_bypass = [2, random.randint(1,1011), random.randint(0,1), random.randint(0,1), 32]
+    digital_bypass = [2, random.randint(1,2), random.randint(0,1), random.randint(0,1), 32]
+
     #MitigationType, BeamDestination.id, BeamClass.id (oldValue), BeamClass.id (newValue), 0
-    #faultid, faultvalue(old), faultvalue(new), allowedclass.id, 0
     ##mitigation = [4, random.randint(1,4), random.randint(1,11), random.randint(1,11), 0]
     #DeviceInputType, DeviceInput.id, oldValue, newValue, 0
-    device_input = [5, random.randint(1,1011), random.randint(0,1), random.randint(0,1), 0]
+#    device_input = [5, random.randint(1,1011), random.randint(0,1), random.randint(0,1), 0]
+    device_input = [5, random.randint(1,2), random.randint(0,1), random.randint(0,1), 0]
+
     #AnalogDeviceType, AnalogDevice.id, oldValue, newValue, 0
-    analog = [6, random.choice(result), 0, 0, 0]
+    #analog = [6, random.choice(result), 0, 0, 0]
+    analog = [6, random.choice(result), 0, 0, random.randint(1, 20)]
+
     test_data = [fault_all, analog_bypass, digital_bypass, device_input, analog]
     pprint.pprint(test_data)
     return test_data
